2dc279eb054a828ee151c3f2d91d05ea21561a03
Noah2211 <noah.decroix3@gmail.com>
2022-01-17 16:48:15 +0100

    done ,try bonus

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
  * --> Attention à bien fermer votre fichier si vous utilisez fopen()
  */
 // TODO Votre code ici.
-
+$myLorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, quasi.";
+file_put_contents('texte.txt', $myLorem);
 
 /**
  * 3. Créez un tableau contenant au moins 10 chaînes de caractère au choix.
@@ -15,20 +16,29 @@
  * --> Attention: les chaînes de caractères doivent s'ajouter à la suite du contenu déjà existant, pas d'écrasement.
  */
 // TODO Votre code ici
-
+$tab = ['1','2','3','4','5','11','12','13','14','15',];
+foreach ($tab as $value) {
+    file_put_contents('texte.txt', $value, FILE_APPEND);
+}
 
 /**
  * 5. Trouvez une solution pour afficher à l'aide d'un simple echo l'extension du fichier index.php
  */
 // TODO Votre code ici
-
+$infos = pathinfo('texte.txt');
+echo $infos['extension'];
 
 
 /**
  * 6. Testez si le fichier 'toto' existe, sil n'existe pas, afficher un texte distant que ce fichier n'existe pas !
  */
 // TODO Votre code ici.
-
+if (file_exists('toto')) {
+    echo "<br><br>"."Le fichier existe";
+}
+else {
+    echo "<br><br>"."Le fichier n'existe pas !";
+}
 
 /**
  * Super bonus.
@@ -36,3 +46,11 @@
  * Attention, il y a un piège avec les pointeurs, et une manière très simple de procéder... réfléchissez...
  */
 // TODO Votre code ici si vous faites le bonus.
+$file = fopen('texte.txt', 'rb');
+if (file_exists('texte.txt')) {
+    while($letters = fgetc($file)) {
+        if ($letters === 'a') {
+            echo "test ";
+        }
+    }
+}
